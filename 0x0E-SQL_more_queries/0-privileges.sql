631584f38e29de0116c3059d407d717608a9078f
KollihTheGuruh <mmike8449@gmail.com>
2023-08-16 13:23:25 -0700

    SQL

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT *
+FROM mysql.user
+WHERE user = 'user_0d_1' OR user = 'user_0d_2';
