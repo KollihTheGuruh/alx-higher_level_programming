4851ecdf856ffd0c5147327cac681da419586655
KollihTheGuruh <mmike8449@gmail.com>
2023-08-17 04:45:56 -0700

    SQL

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,17 @@
--- Lists all privileges of the MySQL users -  user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Create user user_0d_1 if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
+-- Create user user_0d_2 if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
