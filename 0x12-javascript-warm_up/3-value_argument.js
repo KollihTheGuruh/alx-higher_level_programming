19d8ab60e95aefe0092462ed85fc6f2a76c32019
KollihTheGuruh <mmike8449@gmail.com>
2023-09-12 00:53:34 +0300

    3 value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
+
+// Check if at least one argument is provided
+if (process.argv[2]) {
+    // Print the first argument
     console.log(process.argv[2]);
+  } else {
+    // Print "No argument" if no arguments are provided
+    console.log('No argument');
   }
   
