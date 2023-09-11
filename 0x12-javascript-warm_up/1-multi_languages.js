ea8c35f67bb4134a3a3d574ceeb4483a0316ad0b
KollihTheGuruh <mmike8449@gmail.com>
2023-09-11 23:29:11 +0300

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-// Use console.log(...) to print the three lines
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
