a60060f59fecde24b3ff78da12c711528812b66b
KollihTheGuruh <mmike8449@gmail.com>
2023-09-12 21:42:38 +0300

    semistandard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,10 +2,9 @@
 
 // Check if at least one argument is provided
 if (process.argv[2]) {
-    // Print the first argument
-    console.log(process.argv[2]);
-  } else {
-    // Print "No argument" if no arguments are provided
-    console.log('No argument');
-  }
-  
+  // Print the first argument
+  console.log(process.argv[2]);
+} else {
+  // Print "No argument" if no arguments are provided
+  console.log('No argument');
+}
